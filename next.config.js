dd3064f82631c85e449077068488d4968f9d83bc
unknown <picipycurka@gmail.com>
2026-01-20 19:23:05 +0100

    Exclude libsql from webpack bundle

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // App Router is now stable in Next.js 14 - no experimental config needed
-}
+  experimental: {
+    serverComponentsExternalPackages: ["@libsql/client", "libsql"],
+  },
+};
 
 module.exports = nextConfig
